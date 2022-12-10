37a590c48425cd62fa0e607fae050f7f61b97a4b
Mumina Abdumannabova <mumina_uk@hotmail.com>
2022-12-10 13:18:48 +0000

    updated test case TC002

--- src/test_cases/TC002.java
+++ src/test_cases/TC002.java
@@ -5,6 +5,7 @@ public class TC002 {
     public static void main(String[] args) {
         //Test case 2
         //Writing smoke tests......
-
+ /// creating a report
+        // screenshot in failure
     }
 }
